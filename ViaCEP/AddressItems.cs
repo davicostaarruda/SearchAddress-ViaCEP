2c440d18962d02b0b61997b891834e5bb5b554d6
Davi Costa Arruda <daviarruda_@hotmail.com>
2022-07-30 19:02:36 -0300

    SearchAddress ViaCEP v1

---
+++ ViaCEP/AddressItems.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ViaCEP
+{
+    public class AddressItems
+    {
+            public string CEP { get; set; }
+            public string Logradouro { get; set; }
+            public string Complemento { get; set; }
+            public string Bairro { get; set; }
+            public string Localidade { get; set; }
+            public string UF { get; set; }
+            public string Unidade { get; set; }
+            public string IBGE { get; set; }
+            public string Gia { get; set; }
+    }
+}
---
+++ ViaCEP/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ViaCEP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Deseja informar um CEP ou um Endereço?");
+            Console.WriteLine("Digite 1 para CEP.");
+            Console.WriteLine("Digite 2 para Endereço.");
+            var option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Informe o CEP: ");
+                    var cep = Console.ReadLine();
+                    Executar(cep).Wait();
+                break;
+                case 2:
+                    Console.WriteLine("Informe o Endereço (estado/cidade/rua ou avenida com as `/` respectivamente): ");
+                    var address = Console.ReadLine();
+                    Executar(address).Wait();
+                break;
+                default:
+                    Console.WriteLine("Opção inválida, programa encerrado");
+                break;
+            }
+        }
+
+        private static async Task<string> GetJson(string path)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync($"https://viacep.com.br/ws/{path}/json/");
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private static async Task Executar(string path)
+        {
+            var json = await GetJson(path);
+            Console.WriteLine(json);
+        }
+    }
+}
